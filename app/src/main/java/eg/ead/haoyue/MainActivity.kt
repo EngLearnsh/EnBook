fdc8e9f49ef6fef0548907bc28a2b0e12362cc16
EngLearnsh <30802053+EngLearnsh@users.noreply.github.com>
2021-01-26 13:17:14 +0800

    adjustment to basic ui

--- app/src/main/java/eg/ead/haoyue/MainActivity.kt
+++ app/src/main/java/eg/ead/haoyue/MainActivity.kt
@@ -1,12 +1,18 @@
 package eg.ead.haoyue
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import eg.ead.haoyue.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -21,25 +27,44 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         // drawer
-        supportActionBar?.let {
-            it.setDisplayHomeAsUpEnabled(true)
-            it.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
-        }
+        initActionBarDrawer()
 
         // navview
-        binding.navView.setCheckedItem(R.id.setting)
-        binding.navView.setNavigationItemSelectedListener {
-            binding.drawerLayout.closeDrawer(binding.navView)
-            true
-        }
+        initNavItemListener()
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         when (item.itemId) {
             android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
         }
-        return super.onOptionsItemSelected(item)
+        return true
     }
+
+    private fun initActionBarDrawer() {
+        val toggle = ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                binding.toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun initNavItemListener() {
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.setting -> {
+                    val intent = Intent(this, SettingsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.exit -> finish()
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
 }
---
+++ app/src/main/java/eg/ead/haoyue/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package eg.ead.haoyue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
