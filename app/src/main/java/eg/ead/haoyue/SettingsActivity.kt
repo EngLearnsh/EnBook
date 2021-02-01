9ce90e43b8a8b543b747c4d1981d7da22a8a51fb
EngLearnsh <30802053+EngLearnsh@users.noreply.github.com>
2021-02-01 16:57:36 +0800

    settings item

--- app/src/main/java/eg/ead/haoyue/SettingsActivity.kt
+++ app/src/main/java/eg/ead/haoyue/SettingsActivity.kt
@@ -1,10 +1,22 @@
 package eg.ead.haoyue
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import eg.ead.haoyue.databinding.ActivitySettingsBinding
 
 class SettingsActivity : BaseActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+    private val item = listOf("Developer", "Version")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item)
+        binding.listviewSettings.adapter = adapter
+
     }
 }
