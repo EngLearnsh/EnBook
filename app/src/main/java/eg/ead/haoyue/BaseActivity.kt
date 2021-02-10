c2d6183a7f7154307e3dba3631d9a5dfaf60e6a6
EngLearnsh <30802053+EngLearnsh@users.noreply.github.com>
2021-02-11 00:05:15 +0800

    basic activity collector

--- app/src/main/java/eg/ead/haoyue/BaseActivity.kt
+++ app/src/main/java/eg/ead/haoyue/BaseActivity.kt
@@ -5,8 +5,15 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("BaseActivity", javaClass.simpleName)
+        ActivityCollector.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ActivityCollector.removeActivity(this)
     }
 }
